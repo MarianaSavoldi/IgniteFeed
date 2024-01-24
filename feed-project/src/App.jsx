5efcc8105ac3be8bbcee0b27f371b1d2d657ea7f
MarianaSavoldi <marianasavodi123@gmail.com>
2024-01-24 19:39:21 -0300

    feat: create and apply a Sidebar component

--- feed-project/src/App.jsx
+++ feed-project/src/App.jsx
@@ -1,14 +1,22 @@
 import {Post} from "./Post";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import './global.css'
+import styles from './App.module.css'
 
 export function App() {
 
   return (
     <>
       <Header/>
-      <Post author='Me' content='This is my post'/>
-      <Post author='Another person' content='Lorem, ipsum dolor sit amet consectetur adipisicing elit.'/>
+      
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post author='Me' content='This is my post'/>
+          <Post author='Another person' content='Lorem, ipsum dolor sit amet consectetur adipisicing elit.'/>
+        </main>
+      </div>
     </>
 
   )
