561572a8af28236624c4bb8caf108fe4fdd1ef98
MarianaSavoldi <marianasavodi123@gmail.com>
2024-01-24 17:35:17 -0300

    feat: create and apply a Header component

--- feed-project/src/App.jsx
+++ feed-project/src/App.jsx
@@ -1,10 +1,12 @@
 import {Post} from "./Post";
+import { Header } from "./components/Header";
+import './global.css'
 
 export function App() {
 
   return (
     <>
-      <h1>Ignite Feed</h1>
+      <Header/>
       <Post author='Me' content='This is my post'/>
       <Post author='Another person' content='Lorem, ipsum dolor sit amet consectetur adipisicing elit.'/>
     </>
