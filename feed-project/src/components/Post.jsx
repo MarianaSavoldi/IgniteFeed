2caba0b0334a76cb777a11874194fe4127af0130
MarianaSavoldi <marianasavodi123@gmail.com>
2024-01-26 16:01:07 -0300

    feat: add comment form on Post component

--- feed-project/src/components/Post.jsx
+++ feed-project/src/components/Post.jsx
@@ -17,18 +17,26 @@ export function Post() {
         </header>
 
         <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉 <a href="https://github.com/MarianaSavoldi/ignite-feed">jane.design/doctorcare</a></p>
-
-        <p>
-          <a href="https://github.com/MarianaSavoldi/ignite-feed">#novoprojeto</a>
-          {' '}
-          <a href="https://blog.rocketseat.com.br/tag/nlw/">#nlw</a>
-          {' '}
-          <a href="https://www.rocketseat.com.br/">#rocketseat</a>
-        </p>
+          <p>Fala galeraa 👋</p>
+          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+          <p>👉 <a href="https://github.com/MarianaSavoldi/ignite-feed">jane.design/doctorcare</a></p>
+          <p>
+            <a href="https://github.com/MarianaSavoldi/ignite-feed">#novoprojeto</a>
+            {' '}
+            <a href="https://blog.rocketseat.com.br/tag/nlw/">#nlw</a>
+            {' '}
+            <a href="https://www.rocketseat.com.br/">#rocketseat</a>
+          </p>
         </div>
+
+        <form className={styles.commentForm}>
+          <strong>Deixe seu feedback</strong>
+          <textarea placeholder='Deixe um comentário'/>
+          <footer>
+            <button type='submit'>Publicar</button>
+          </footer>
+        </form>
+
       </article>
     </>
   )
